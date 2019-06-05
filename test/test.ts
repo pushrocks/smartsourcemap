db1412d5c1a5baf261dbeb7c0344fd8ee64a0516
Phil Kunz <gitlab@philkunz.com>
2019-06-05 13:58:47 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,7 @@
 import { expect, tap } from '@pushrocks/tapbundle';
+delete process.env.CLI_CALL_TSRUN;
 import '../ts/index';
+
 
 tap.test('first test', async () => {
   console.log('should create a sourcemap');
